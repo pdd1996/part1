67caed50a727af98cda218ee0feb85435f203d90
paddy <pdd337816464@163.com>
2023-04-25 18:40:46 +0800

    修改 - 完成计算器

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import Hello from "./Hello";
+import {useState} from "react";
+import Display from "./components/Display";
+import Button from "./components/Button";
 
-const App = (props) => {
-  const { count } = props;
+const App = () => {
+  const [ counter, setCounter ] = useState(0);
+
+  const addByOne = () => {
+    return setCounter(counter + 1)
+  }
+  const deleteByOne = () => {
+    return setCounter(counter - 1)
+  }
+  const zero = () => {
+    return setCounter(0)
+  }
 
   return (
     <div>
       <h1>Greetings</h1>
-     <div>{ count }</div>
+      <Display counter={counter} />
+      <Button onClick={addByOne} text="+ 1"></Button>
+      <Button onClick={deleteByOne} text="- 1"></Button>
+      <Button onClick={zero} text="0"></Button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-let count = 1;
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const refresh = () => root.render(
+root.render(
   <React.StrictMode>
-    <App count={count} />
+    <App />
   </React.StrictMode>
 );
-
-setInterval(() => {
-  refresh()
-  count += 1
-}, 1000)
