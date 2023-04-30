18dc930bdce9b0cf6fd3d9229febf9ea1d2d0856
paddy <pdd337816464@163.com>
2023-04-30 15:35:09 +0800

    修改 - App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import {useState} from "react";
-import History from "./components/History";
-import Button from "./components/Button";
+import { useState } from 'react'
 
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
 
-function App() {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
-  const [allClicks, setAll] = useState([])
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(7).fill(0))
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1
-    }
-    setClicks(newClicks)
+  const handleClickForNext = () => {
+    const num = Math.round(Math.random() * (anecdotes.length - 1));
+    setSelected(num)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
+  const handleClickForVote =() => {
+    const newPoints = points.slice()
+    newPoints[selected] ++
+    setPoints(newPoints)
   }
+
+  const maxPoint = Math.max(...points)
+  const index = points.indexOf(maxPoint)
 
   return (
     <div>
-      {clicks.left}
-      <Button onClick={handleLeftClick} text='left' />
-      {clicks.right}
-      <Button onClick={handleRightClick} text='right' />
-      <History allClicks={allClicks} />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={handleClickForVote}>Vote</button>
+      <button onClick={handleClickForNext}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[index]}</p>
     </div>
   )
 }
 
-export default App;
+export default App
